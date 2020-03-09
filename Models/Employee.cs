d20fa8c9cbed93eef2078ada123e2333b1532387
Saz <sazi.nyathi@lexisnexis.co.za>
2020-03-09 22:55:32 +0200

    DEV: Created a new project

---
+++ Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Models
+{
+    [Table("Employee")]
+    public class Employee
+    {
+        [Required]
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int EmployeeId { get; set; }
+
+        [Required(ErrorMessage = "Please provide Employee Name ")]
+        [MaxLength(50)]
+        public string EmployeeName { get; set; }
+
+        [Required(ErrorMessage = "Please provide Employee Surname ")]
+        [MaxLength(50)]
+        public string EmployeeSurname { get; set; }
+        
+    }
+}
---
+++ WebAPI.Repositories/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+using WebAPI.Models;
+
+namespace WebAPI.Repositories
+{
+   public class ApplicationDbContext:DbContext
+    {
+      
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
